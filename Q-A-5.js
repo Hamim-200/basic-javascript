3d1172c18e72f6f2867fa93efc143fef8cd1618c
Hamim Ahmed <hamim.ahmed484@gmail.com>
2022-08-03 21:49:23 +0600

    2nd commit

---
+++ Q-A-5.js
@@ -0,0 +1,4 @@
+// Changing variable Value 
+var number = 10;
+number = 15;
+console.log(number);
---
+++ basicQ-A.js
@@ -0,0 +1,15 @@
+/***************
+ * 1.What Is JavaScript?
+ * Ans: JavaScript is a scripting or programming language that allows you to implement complex features on web pages.
+ * 
+ * 2.How does JavaScript work?
+ * Ans:The source code is passed through a program called a compiler, which translate it into bytecode that the machine understand and can execute. In contrast,JavaScript has no complilation step.instead an interpreter in the browser reads over the JavaScript code,interprets each line, and runs it.
+ * 
+ * 3.What is variable ?
+ * Ans: Variable means anything that can vary. In JavaScript, a variable store the data value that can be changed later on.
+ * 
+ * 4.How to declare variable in javascript?
+ * Ans: Variables are containers for storing data.Variables are declare with the keyword 'Var'.Example: var number = 10;
+ * 
+ * 5.--Other Packege-- 
+ ***************/ 
